497a18924340550aa73433b1b3e1cc80c23db47d
Abhishek Ranjan <37826175+Abhishek-Rnjn@users.noreply.github.com>
2024-10-15 11:39:32 +0000

    added comment

--- aiagents/config/config.py
+++ aiagents/config/config.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv, find_dotenv
 from langchain_openai import AzureChatOpenAI, ChatOpenAI
 import panel as pn
 
-
+# changing these defaults
 class Initialize:
 
     diagrams = {
